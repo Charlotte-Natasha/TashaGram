7b87056d103705f01a2219f9f502229fce07d04d
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-03 11:07:31 +0200

    added signup authentication

--- gram/views.py
+++ gram/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'gram/index.html', {})
 
 def sign_up(request):
-    
-    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/login')
+    else:
+        form = RegisterForm() 
 
+        messages.success(request, "Your account has been successfully created")
     return render(request, 'registration/signup.html', {'form':form})    
