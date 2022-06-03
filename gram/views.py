d49dae7f85adea3035fffb96636d17d5514383db
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-03 09:23:03 +0200

    created signup form

--- gram/views.py
+++ gram/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 def index(request):
     return render(request, 'gram/index.html', {})
 
 def sign_up(request):
-    return render(request, 'gram/signup.html', {})    
+    
+    form = RegisterForm()
+
+    return render(request, 'registration/signup.html', {'form':form})    
